bc01c10f821b4a721abfad75fda25136913a9dd3
Tom Rogers <tomrogers@tomrogers.local>
2021-12-24 17:02:22 +1100

    updated ui to now show charts/tables

--- stonks_app/ui.R
+++ stonks_app/ui.R
@@ -6,6 +6,21 @@ library(plotly)
 library(tableHTML)
 source("functions.R")
 
+
+annual_returns_text <- HTML("The annual return for each of your selected stocks.")
+
+annual_risk_text <- HTML("The annual risk (variance) for each of your selected stocks.")
+
+optimal_portfolio_text <- HTML("Your optimal portfolio with the percentage allocation for each stock. This is the portfolio that has had the greatest return relative to its risk.")
+
+minimum_variance_portfolio_text <- HTML("The portfolio with the least risk. This is likely to have lower returns.")
+
+candidate_portfolios_text <- HTML("Each point here is an alternative portfolio showing it's risk and return. The optimal portfolio is the shaded point. Hover to see the breakdown of stocks.")
+
+price_plot_text <- HTML("The historical returns for each stock.")
+
+stock_correlations_text <- HTML("The correlations between returns for each pair of stocks.")
+
 disclaimer_string <- "The information on this website is for general information only and should not be taken as financial advice nor any professional advice from the website owner. We are not liable for any loss caused from information provided directly or indirectly. The tools on this website are illustrative and should not be taken as a substitute for professional advice. All reasonable care has been taken in development; however, we provide absolutely no warranty."
 
 
@@ -50,8 +65,11 @@ bootstrapPage(
                         selected = 1
                     ),
                     actionButton("go", "Start the Analysis")
-                )
+                ),
                 # disclaimer
+                box(width = NULL, title = "Disclaimer",
+                    p(disclaimer_string, class = "text-muted", style = "font-size:10px")
+                )
             ),
             div(class="col-md-9",
                 img(src = "stonks-banner-thin.png", class="d-none d-md-block", alt="Responsive image", 
@@ -60,8 +78,57 @@ bootstrapPage(
             conditionalPanel(
                 condition = "input.go",
                 
-            )    
+               fluidRow(
+                   column(
+                       width = 12,
+                    box(style = "overflow-x:scroll; max-height: 300px; position:relative; align: centre",
+                        width = NULL, title = "Optimal Portfolio",
+                        p(optimal_portfolio_text, class = "text-muted"),
+                        includeCSS('www/mycss.css'),
+                        uiOutput("max_sharpe_ratio_table")
+                    )
+                )
                 
+            ),
+            
+            fluidRow(
+                column(
+                    width = 6,
+                    box(width = NULL, title = "Annual Return",
+                        p(annual_returns_text, class = "text-muted"),
+                        uiOutput("annual_returns")
+                    ),
+                    box(width = NULL, title = "Annual Risk",
+                        p(annual_risk_text, class = "text-muted"),
+                        uiOutput("annual_risk")
+                    ),
+                    box(style = "overflow-x:scroll; max-height: 300px; position:relative; align: centre",
+                        width = NULL, title = "Minimum Variance Portfolio",
+                        uiOutput("min_risk_table")
+                    )
+                ),
+                column(width = 6,
+                       box(width = NULL, title = "Candidate Portfolios",
+                           p(candidate_portfolios_text, class = "text-muted"),
+                           plotlyOutput("portfolio_plot")
+                       )
+                )
+            ),
+            fluidRow(
+                column(width = 6,
+                       box(width = NULL, title = "Price Plot",
+                           p(price_plot_text, class = "text-muted"),
+                           plotlyOutput('relative_returns')
+                       )
+                ),
+                column(width = 6,
+                       box(width = NULL, title = "Stock Correlations",
+                           p(stock_correlations_text, class = "text-muted"),
+                           plotOutput("correlation_plot")
+                       )
+                    )
+                )
+            )
         )
     )
 )
